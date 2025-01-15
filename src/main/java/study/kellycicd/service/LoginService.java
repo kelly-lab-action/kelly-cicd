84062a58a55ecf8d7e43c105f1a8073a70b1138e
kelly6bf <kelly6bf@gmail.com>
2025-01-16 03:11:30 +0900

    로그인 기능 로깅 로직 추가

--- src/main/java/study/kellycicd/service/LoginService.java
+++ src/main/java/study/kellycicd/service/LoginService.java
@@ -1,11 +1,19 @@
 package study.kellycicd.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class LoginService {
 
     public boolean login(final String username, final String password) {
-        return username.equals("kelly") && password.equals("kellyPw");
+        if (username.equals("kelly") && password.equals("kellyPw")) {
+            log.info("로그인 성공 : {}", username);
+            return true;
+        }
+
+        log.info("로그인 실패 : {}", username);
+        return false;
     }
 }
